df0712cdcbc9b17bb24c055c578d8291235df315
weify <weiweiplease@gmail.com>
2012-04-28 23:14:39 +0800

    增加SQlite功能模块

---
+++ iew/sqlite.py
@@ -0,0 +1,15 @@
+'''
+Created on 2012-4-28
+
+@author: Wei
+'''
+
+class DBManager(object):
+    '''
+    classdocs
+    '''
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
